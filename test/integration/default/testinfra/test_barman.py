62ad000da7ab65d57838a0e5ecc117c282ff54f4
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-07-08 09:00:34 +0100

    Rename test to match what the config files do.

--- test/integration/default/testinfra/test_barman.py
+++ test/integration/default/testinfra/test_barman.py
@@ -6,13 +6,13 @@ def test_config_file(host):
     assert barman.contains("log_level")
     assert barman.contains("gzip")
 
-def test_host_config_file_for_pgsql1(host):
+def test_host_config_file_for_streaming(host):
     barman = host.file("/etc/barman.d/pgsql1.conf")
     assert barman.contains("pgsql1")
     assert barman.contains("barman")
     assert barman.contains("postgres")
 
-def  test_host_config_file_for_pgsql2(host):
+def  test_host_config_file_for_rsync(host):
     barman = host.file("/etc/barman.d/pgsql2.conf")
     assert barman.contains("pgsql2")
     assert barman.contains("link")
