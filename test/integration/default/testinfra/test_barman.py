850bf3270736599d8bcfed16e23dc448ac222d66
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-07-08 08:58:27 +0100

    Add test for the two types of config files

--- test/integration/default/testinfra/test_barman.py
+++ test/integration/default/testinfra/test_barman.py
@@ -6,7 +6,13 @@ def test_config_file(host):
     assert barman.contains("log_level")
     assert barman.contains("gzip")
 
-def test_host_config_file(host):
+def test_host_config_file_for_pgsql1(host):
+    barman = host.file("/etc/barman.d/pgsql1.conf")
+    assert barman.contains("pgsql1")
+    assert barman.contains("barman")
+    assert barman.contains("postgres")
+
+def  test_host_config_file_for_pgsql2(host):
     barman = host.file("/etc/barman.d/pgsql1.conf")
     assert barman.contains("pgsql1")
     assert barman.contains("barman")
